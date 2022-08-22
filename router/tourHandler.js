eb1c084b3c4f0dcc29769813f55c03fe51f22f68
Aniksaha-ui <sahaanik1045@gmail.com>
2022-08-22 21:28:13 +0600

    tour description api done

--- router/tourHandler.js
+++ router/tourHandler.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const Tour = require("../model/tour");
 require("dotenv").config();
 
+//upcomeing tour
 router.get("/upcomeing", async (req, res) => {
   const tour = await Tour.find({ status: "upcomeing" });
   if (tour.length > 0) {
@@ -13,6 +14,8 @@ router.get("/upcomeing", async (req, res) => {
     res.send({ message: "data not found" });
   }
 });
+
+//popular tour
 router.get("/popular", async (req, res) => {
   const tour = await Tour.find({ status: "popular" });
   if (tour.length > 0) {
@@ -20,6 +23,14 @@ router.get("/popular", async (req, res) => {
   } else {
     res.send({ message: "data not found" });
   }
+});
+
+//tour description
+
+router.get("/description/:id", async (req, res) => {
+  const id = req.params.id;
+  const tourDescription = await Tour.find({ _id: id });
+  res.send(tourDescription);
 });
 
 module.exports = router;
